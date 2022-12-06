6d8a92af245160ccdd9c2de3253b893cbd2741d2
Yasin Zähringer <yasin@paradime.io>
2022-12-06 18:44:38 +0000

    Revert non-local behaviour since it is not conforming with jinja

--- exec/context.go
+++ exec/context.go
@@ -32,23 +32,8 @@ func (ctx *Context) Get(name string) interface{} {
 	}
 }
 
-func (ctx *Context) Overwrite(name string, value interface{}) bool {
-	if _, hasKey := ctx.data[name]; hasKey {
-		ctx.data[name] = value
-		return true
-	} else if ctx.parent != nil {
-		return ctx.parent.Overwrite(name, value)
-	} else {
-		return false
-	}
-}
-
-// Set first tries to overwrite the variable in a outer scope before adding it here.
-// If that doesn't work, we'll set the variable locally.
 func (ctx *Context) Set(name string, value interface{}) {
-	if !ctx.Overwrite(name, value) {
-		ctx.data[name] = value
-	}
+	ctx.data[name] = value
 }
 
 func (ctx *Context) Inherit() *Context {
